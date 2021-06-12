6f01c90a74465e7f24d8cbd4b697b58b1d9c4e60
Prathiksha Poojary <47886532+cprathiksha@users.noreply.github.com>
2021-06-12 18:24:27 +0530

    Add files via upload

---
+++ login.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class login{
+	
+	String f_name, l_name, u_name, password, country;
+	int age;
+	String user_name, user_password;
+	
+	public void input()
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the First name :");
+		f_name = in.next();
+		System.out.println("Enter the Last name :");
+		l_name = in.next();
+		System.out.println("Enter the User name :");
+		u_name = in.next();
+		System.out.println("Enter the Password :");
+		password = in.next();
+		System.out.println("Enter the age :");
+		age = in.nextInt();
+		System.out.println("Enter value for country :");
+		country = in.next();
+		
+	}
+	public void check()
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("\n To Login \n");
+		System.out.println("Enter user name :");
+		user_name = in.next();
+		System.out.print("Enter the password :");
+		user_password = in.next();
+		
+		if(user_name.equals(u_name) && user_password.equals(password))
+		{
+			System.out.print("User "+u_name+" logged in successfully from "+country);
+		}
+		else
+		{
+			System.out.print("Invalid entry!");
+		}
+		
+	}
+	public static void main(String[] args)
+	{
+		
+		login obj = new login();
+		obj.input();
+		obj.check();
+		
+			
+	}
+}
